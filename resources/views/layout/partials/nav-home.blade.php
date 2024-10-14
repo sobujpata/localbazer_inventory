d053249982890c58942f5ce10ec67c3ee0626a44
sobujpata <mdsalimrezaspi@gmail.com>
2024-10-14 17:39:04 +0600

    change about

--- resources/views/layout/partials/nav-home.blade.php
+++ resources/views/layout/partials/nav-home.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar sticky-top shadow-sm navbar-expand-lg navbar-light py-2">
     <div class="container">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="{{url('/')}}">
             <img class="img-fluid" src="{{asset('/images/logo.png')}}" alt="" width="96px">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#header01" aria-controls="header01" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,7 +8,7 @@
         </button>
         <div class="collapse navbar-collapse" id="header01">
             <ul class="navbar-nav ms-auto mt-3 mt-lg-0 mb-3 mb-lg-0 me-4">
-                <li class="nav-item me-4"><a class="nav-link" href="#">About</a></li>
+                <li class="nav-item me-4"><a class="nav-link" href="{{url('/')}}">About</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{url('/testmonials')}}">Testimonials</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{url('/contact')}}">Contact</a></li>
                 <li class="nav-item me-4"><a class="nav-link" href="{{url('/products-list')}}">Products</a></li>
